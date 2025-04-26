fb1d9e93c13996744b4ddf98c636bc8203e4208f
Your Name <abhisoldiers@gmail.com>
2025-04-26 12:37:35 +0530

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;        // default port 3000
+
+// connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Rabbit API!");
+});
+
+// API Routes
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
