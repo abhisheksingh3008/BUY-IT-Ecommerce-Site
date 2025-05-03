44830ca2ebaebf86a6aa785d04823b71ec04ca67
Your Name <abhisoldiers@gmail.com>
2025-05-03 15:38:19 +0530

    Third Commit

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import heroImg from "../../assets/rabbit-hero.webp";
+import heroImg from "../../assets/rabbit-hero.jpg";
 const Hero = () => {
   return (
     <section className="relative">
@@ -10,12 +10,9 @@ const Hero = () => {
          />
          <div className="absolute inset-0 bg-black bg-opacity-5 flex items-center justify-center">
             <div className="text-center text-white p-6">
-                <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
-                    Vacation <br /> Ready
-                </h1>
-                <p className="text-sm tracking-tighter md:text-lg mb-6">
-                    Explore our vacation-ready outfits with fast worldwide shipping.
-                </p>
+                <h1 className="text-4xl md:text-9xl font-semibold tracking-tighter uppercase mb-4">
+                    Urban <br /> life
+                </h1> 
                 <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg">
                 Shop Now
                 </Link>
